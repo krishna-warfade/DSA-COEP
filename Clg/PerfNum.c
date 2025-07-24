08b50c78ca7fa4315e51a9024d80ff9480c5c826
Krishna Warfade <krishnawarfade@gmail.com>
2025-07-24 19:42:07 +0530

    1st qs

---
+++ Clg/PerfNum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    scanf("%d", &num);
+    for (int i = 1; i < num; i++)
+    {
+        if(num % i == 0) sum += i;
+    }
+    if (sum == num)
+    {
+        printf("Perfect Number\n");
+    } else
+    {
+        printf("Not a Perfect Number\n");
+    }  
+    
+    return 0;
+}
