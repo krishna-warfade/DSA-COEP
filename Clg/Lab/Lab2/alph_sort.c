4783fc529c39b00923fe461720da3a64cfab9e06
Krishna Warfade <krishnawarfade@gmail.com>
2025-08-03 00:04:31 +0530

    lab2 file

---
+++ Clg/Lab/Lab2/alph_sort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *fpr, *fpw;
+    char line[100];
+    char *words[100];
+    int count = 0;
+
+    fpr = fopen("read.txt", "r");
+    if (fpr == NULL) printf("Error opening file");
+    fpw = fopen("sorted.txt", "w");
+    if (fpw == NULL) printf("Error opening file");
+
+    fclose(fpr);
+    fclose(fpw);
+    return 0;
+}
