986bae659fa724251c8152f5835aa6f2a0739794
Krishna Warfade <krishnawarfade@gmail.com>
2025-08-06 08:59:27 +0530

    added assgs

---
+++ Clg/Lab/Assignments/assg2.c
@@ -0,0 +1,3 @@
+/*Compare time complexities of 2 sorting algorithms. Create a set of integers varying in thousands in a file & 
+ *sort the file using 2 sorting algorithms.
+ *Plot graph of time taken by both the programs using : gnuplot*/
