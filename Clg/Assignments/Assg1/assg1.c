acb6a6de1465fa5578e32deaf55395ee8a0c278f
Krishna Warfade <krishnawarfade@gmail.com>
2025-08-09 22:41:49 +0530

    changes, assg submitted

--- Clg/Assignments/Assg1/assg1.c
+++ Clg/Assignments/Assg1/assg1.c
@@ -5,6 +5,10 @@ int main() {
     double arr[] = {1.0, 2.0, 2.0, 3.0, 4.0, 4.0, 5.0, 6, 6.0, 7.0, 8.0, 9.0, 9.0, 10.0, 10.0, 11.0, 12.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 20.0, 21.0, 22.0, 23.0, 24};
     int n = sizeof(arr) / sizeof(arr[0]);
     
+    printf("Array before removing duplicates: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", arr[i]);
+    }
     int i = 0;
     for (int j = 1; j < n; j++) {
         if (arr[j] != arr[i]) {
@@ -12,10 +16,12 @@ int main() {
             arr[i] = arr[j];            
         }
     }
-    printf("Array after removing duplicates: ");
+    printf("\n");
+    printf("Array after removing duplicates: \n");
     for (int k = 0; k <= i; k++) {
         printf("%.2f ", arr[k]);
     }
+    printf("\n");
     
     return 0;
 }
